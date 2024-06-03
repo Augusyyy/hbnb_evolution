5a9058b6d61bea605875aa1e179b7d55cf90d69b
Augusyyy <8851@holbertonstudents.com>
2024-06-04 00:15:10 +1000

    user/user_id

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask
+from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
 
+api = Api(app, version='1.0', title='hbnb_evolution API', description='A hbnb_evolution project API')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+@api.route('/users')
+class UserList(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+@api.route('/users/<string:user_id>')
+class UserParam(Resource):
+    def get(self, user_id):
+        pass
+
+    def delete(self, user_id):
+        pass
+
+    def put(self, user_id):
+        pass
+    
 
 if __name__ == '__main__':
     app.run()
