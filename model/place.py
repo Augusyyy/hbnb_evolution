154e3953f22a17af7729ed18331270938365d8f9
Augusyyy <8851@holbertonstudents.com>
2024-06-03 23:37:01 +1000

    place

--- model/place.py
+++ model/place.py
@@ -5,128 +5,128 @@ from datetime import datetime
 class Place:
     def __init__(self, host_user_id, city_id, name, description, address, latitude, longitude, number_of_rooms,
                  bathrooms, price_per_night, max_guests):
-        self._id = str(uuid.uuid4())
-        self._host_user_id = host_user_id
-        self._city_id = city_id
-        self._name = name
-        self._description = description
-        self._address = address
-        self._latitude = latitude
-        self._longitude = longitude
-        self._number_of_rooms = number_of_rooms
-        self._bathrooms = bathrooms
-        self._price_per_night = price_per_night
-        self._max_guests = max_guests
-        self._created_at = datetime.now().timestamp()
-        self._updated_at = self._created_at
+        self.__id = str(uuid.uuid4())
+        self.__host_user_id = host_user_id
+        self.__city_id = city_id
+        self.__name = name
+        self.__description = description
+        self.__address = address
+        self.__latitude = latitude
+        self.__longitude = longitude
+        self.__number_of_rooms = number_of_rooms
+        self.__bathrooms = bathrooms
+        self.__price_per_night = price_per_night
+        self.__max_guests = max_guests
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     @property
     def id(self):
-        return self._id
+        return self.__id
 
     @property
     def host_user_id(self):
-        return self._host_user_id
+        return self.__host_user_id
 
     @host_user_id.setter
     def host_user_id(self, value):
-        self._host_user_id = value
-        self._updated_at = datetime.now().timestamp()
+        self.__host_user_id = value
+        self.updated_at = datetime.now()
 
     @property
     def city_id(self):
-        return self._city_id
+        return self.__city_id
 
     @city_id.setter
     def city_id(self, value):
-        self._city_id = value
-        self._updated_at = datetime.now().timestamp()
+        self.__city_id = value
+        self.updated_at = datetime.now()
 
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @name.setter
     def name(self, value):
-        self._name = value
-        self._updated_at = datetime.now().timestamp()
+        self.__name = value
+        self.updated_at = datetime.now()
 
     @property
     def description(self):
-        return self._description
+        return self.__description
 
     @description.setter
     def description(self, value):
-        self._description = value
-        self._updated_at = datetime.now().timestamp()
+        self.__description = value
+        self.updated_at = datetime.now()
 
     @property
     def address(self):
-        return self._address
+        return self.__address
 
     @address.setter
     def address(self, value):
-        self._address = value
-        self._updated_at = datetime.now().timestamp()
+        self.__address = value
+        self.updated_at = datetime.now()
 
     @property
     def latitude(self):
-        return self._latitude
+        return self.__latitude
 
     @latitude.setter
     def latitude(self, value):
-        self._latitude = value
-        self._updated_at = datetime.now().timestamp()
+        self.__latitude = value
+        self.updated_at = datetime.now()
 
     @property
     def longitude(self):
-        return self._longitude
+        return self.__longitude
 
     @longitude.setter
     def longitude(self, value):
-        self._longitude = value
-        self._updated_at = datetime.now().timestamp()
+        self.__longitude = value
+        self.updated_at = datetime.now()
 
     @property
     def number_of_rooms(self):
-        return self._number_of_rooms
+        return self.__number_of_rooms
 
     @number_of_rooms.setter
     def number_of_rooms(self, value):
-        self._number_of_rooms = value
-        self._updated_at = datetime.now().timestamp()
+        self.__number_of_rooms = value
+        self.updated_at = datetime.now()
 
     @property
     def bathrooms(self):
-        return self._bathrooms
+        return self.__bathrooms
 
     @bathrooms.setter
     def bathrooms(self, value):
-        self._bathrooms = value
-        self._updated_at = datetime.now().timestamp()
+        self.__bathrooms = value
+        self.updated_at = datetime.now()
 
     @property
     def price_per_night(self):
-        return self._price_per_night
+        return self.__price_per_night
 
     @price_per_night.setter
     def price_per_night(self, value):
-        self._price_per_night = value
-        self._updated_at = datetime.now().timestamp()
+        self.__price_per_night = value
+        self.updated_at = datetime.now()
 
     @property
     def max_guests(self):
-        return self._max_guests
+        return self.__max_guests
 
     @max_guests.setter
     def max_guests(self, value):
-        self._max_guests = value
-        self._updated_at = datetime.now().timestamp()
+        self.__max_guests = value
+        self.updated_at = datetime.now()
 
     @property
     def created_at(self):
-        return self._created_at
+        return self.created_at
 
     @property
     def updated_at(self):
-        return self._updated_at
+        return self.updated_at
