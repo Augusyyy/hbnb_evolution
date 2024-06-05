aaa64547226c7f778d4191b89e1400da30032279
Augusyyy <8851@holbertonstudents.com>
2024-06-06 00:50:44 +1000

    DataManager def

--- data/DataManager.py
+++ data/DataManager.py
@@ -160,12 +160,156 @@ class DataManager(IPersistenceManager):
 
         else:
             raise TypeError("Unsupported entity type")
-        
+
     def update(self, entity):
-        pass
+        if isinstance(entity, User):
+            for idx, user in enumerate(self.__user['User']):
+                if user['id'] == entity.id:
+                    updated_user = {
+                        'id': entity.id,
+                        'created_at': entity.created_at,
+                        'updated_at': entity.updated_at,
+                        'first_name': entity.first_name,
+                        'last_name': entity.last_name,
+                        'email': entity.email,
+                        'password': entity.password
+                    }
+                    self.__user['User'][idx] = updated_user
+                    filename = 'data/' + EntityType.USER.value + '.json'
+                    with open(filename, "w") as f:
+                        f.write(json.dumps(self.__user, indent=4))
+                    return updated_user
+            return None
+
+        elif isinstance(entity, Review):
+            for idx, review in enumerate(self.__review['Review']):
+                if review['id'] == entity.id:
+                    updated_review = {
+                        'id': entity.id,
+                        'created_at': entity.created_at,
+                        'updated_at': entity.updated_at,
+                        'commentor_user_id': entity.commentor_user_id,
+                        'place_id': entity.place_id,
+                        'feedback': entity.feedback,
+                        'rating': entity.rating
+                    }
+                    self.__review['Review'][idx] = updated_review
+                    filename = 'data/' + EntityType.REVIEW.value + '.json'
+                    with open(filename, "w") as f:
+                        f.write(json.dumps(self.__review, indent=4))
+                    return updated_review
+            return None
+
+        elif isinstance(entity, Place):
+            for idx, place in enumerate(self.__places['Place']):
+                if place['id'] == entity.id:
+                    updated_place = {
+                        'id': entity.id,
+                        'created_at': entity.created_at,
+                        'updated_at': entity.updated_at,
+                        'name': entity.name,
+                        'description': entity.description,
+                        'address': entity.address,
+                        'latitude': entity.latitude,
+                        'longitude': entity.longitude,
+                        'number_of_rooms': entity.number_of_rooms,
+                        'bathrooms': entity.bathrooms,
+                        'price_per_night': entity.price_per_night,
+                        'max_guests': entity.max_guests
+                    }
+                    self.__places['Place'][idx] = updated_place
+                    filename = 'data/' + EntityType.PLACE.value + '.json'
+                    with open(filename, "w") as f:
+                        f.write(json.dumps(self.__places, indent=4))
+                    return updated_place
+            return None
+
+        elif isinstance(entity, Amenity):
+            for idx, amenity in enumerate(self.__amenity['Amenity']):
+                if amenity['id'] == entity.id:
+                    updated_amenity = {
+                        'id': entity.id,
+                        'created_at': entity.created_at,
+                        'updated_at': entity.updated_at,
+                        'name': entity.name
+                    }
+                    self.__amenity['Amenity'][idx] = updated_amenity
+                    filename = 'data/' + EntityType.AMENITY.value + '.json'
+                    with open(filename, "w") as f:
+                        f.write(json.dumps(self.__amenity, indent=4))
+                    return updated_amenity
+            return None
+
+        elif isinstance(entity, Country):
+            for idx, country in enumerate(self.__countries['Country']):
+                if country['id'] == entity.id:
+                    updated_country = {
+                        'id': entity.id,
+                        'created_at': entity.created_at,
+                        'updated_at': entity.updated_at,
+                        'name': entity.name,
+                        'code': entity.code
+                    }
+                    self.__countries['Country'][idx] = updated_country
+                    filename = 'data/' + EntityType.COUNTRY.value + '.json'
+                    with open(filename, "w") as f:
+                        f.write(json.dumps(self.__countries, indent=4))
+                    return updated_country
+            return None
+
+        elif isinstance(entity, City):
+            for idx, city in enumerate(self.__cities['City']):
+                if city['id'] == entity.id:
+                    updated_city = {
+                        'id': entity.id,
+                        'created_at': entity.created_at,
+                        'updated_at': entity.updated_at,
+                        'country_id': entity.country_id,
+                        'name': entity.name
+                    }
+                    self.__cities['City'][idx] = updated_city
+                    filename = 'data/' + EntityType.CITY.value + '.json'
+                    with open(filename, "w") as f:
+                        f.write(json.dumps(self.__cities, indent=4))
+                    return updated_city
+            return None
+
+        else:
+            raise TypeError("Unsupported entity type")
 
     def get(self, entity_id, entity_type):
-        pass
+        if entity_type == EntityType.USER:
+            for user in self.__user['User']:
+                if user['id'] == entity_id:
+                    return user
+            return None
+        elif entity_type == EntityType.REVIEW:
+            for review in self.__review['Review']:
+                if review['id'] == entity_id:
+                    return review
+            return None
+        elif entity_type == EntityType.PLACE:
+            for place in self.__places['Place']:
+                if place['id'] == entity_id:
+                    return place
+            return None
+        elif entity_type == EntityType.AMENITY:
+            for amenity in self.__amenity['Amenity']:
+                if amenity['id'] == entity_id:
+                    return amenity
+            return None
+        elif entity_type == EntityType.COUNTRY:
+            for country in self.__countries['Country']:
+                if country['id'] == entity_id:
+                    return country
+            return None
+        elif entity_type == EntityType.CITY:
+            for city in self.__cities['City']:
+                if city['id'] == entity_id:
+                    return city
+            return None
+        else:
+            raise TypeError("Unsupported entity type")
 
 
 
