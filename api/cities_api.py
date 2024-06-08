036948a8a0f655f1b1577d3993544c960062b406
Augusyyy <8851@holbertonstudents.com>
2024-06-09 02:47:29 +1000

    cities_api

--- api/cities_api.py
+++ api/cities_api.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_restx import Resource, Api, fields
 from flask import Flask, jsonify, request
-from api import cities_api
+from api import cities_api, api
 from data.DataManager import DataManager, EntityType
 from model.city import City
 
@@ -35,6 +35,35 @@ class NewCity(Resource):
 
         if not country_exists:
             return {'message': 'Invalid country ID'}, 400
+
+        countries = data_manager.get_list(EntityType.COUNTRY)
+        country_exists = False
+        for country in countries:
+            if country['id'] == country_id:
+                country_exists = True
+                break
+
+        if not country_exists:
+            return {'message': 'Invalid country ID'}, 400
+
+        cities = data_manager.get_list(EntityType.CITY)
+        for city in cities:
+            if city['name'] == name and city['country_id'] == country_id:
+                return {'message': 'City name already exists in this country'}, 409
+
+        global city_id_counter
+        city_id = str(city_id_counter)
+        city_id_counter += 1
+        new_city = {
+            'id': city_id,
+            'name': name,
+            'country_id': country_id,
+            'created_at': datetime.utcnow().isoformat(),
+            'updated_at': datetime.utcnow().isoformat()
+        }
+        data_manager.save(new_city, EntityType.CITY)
+        return new_city, 201
+
 
     @cities_api.doc('Retrieve all cities.')
     def get(self):
@@ -89,26 +118,16 @@ class Cities(Resource):
             if city['name'] == name and city['country_id'] == country_id and city['id'] != country_id:
                 return {'message': 'City name already exists in this country'}, 409
 
-        city_update['name'] == name
-        city_update['country_id'] = country_id
-        city_update['updated_at'] = datetime.utcnow().isoformat()
+        updated_city = City(name, country_id)
+        updated_city.id = city_id
 
         data_manager.update(city_update)
         return city_update, 200
 
     @cities_api.doc('Delete a specific city')
     def delete(self, city_id):
-        cities = data_manager.get_list(EntityType.CITY)
-        city_exists = False
-        updated_cities = []
-
-        for city in cities:
-            if city['id'] == city_id:
-                city_exists = True
-            else:
-                updated_cities.append(city)
-
-        if city_exists:
-            data_manager.update(updated_cities)
-            return {'message': 'City deleted successfully'}, 200
-        return {'message': 'City not found'}, 404
+        result = data_manager.delete(city_id, EntityType.USER)
+        if result is None:
+            api.abort(404, message='User not found')
+        else:
+            return jsonify({"message": "User deleted successfully"})
